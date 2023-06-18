8cfa4c667827939d1806ad8c86263833cd9946b5
Dima Klyushkin <KlyushkinDimaJob@gmail.com>
2023-06-18 22:09:14 +0300

    fix

--- src/test/java/ru/levelp/at/homework3/DeleteMailTest.java
+++ src/test/java/ru/levelp/at/homework3/DeleteMailTest.java
@@ -2,61 +2,18 @@ package ru.levelp.at.homework3;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-import static ru.levelp.at.utils.SleepUtils.sleep;
 
 import java.time.LocalDateTime;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class DeleteMessageTest {
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeMethod() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        driver.quit();
-    }
-
+public class DeleteMailTest extends MailTestBase {
     @Test
     public void testDeleteMessage() {
-        //Войти в почту
-        driver.navigate().to("https://mail.ru/");
-        WebElement btnAuthorization = driver.findElement(By.className("ph-login"));
-        btnAuthorization.click();
-
-        sleep(1000);
-        WebElement frameAuthorization = driver.findElement(By.className("ag-popup__frame__layout__iframe"));
-        driver.switchTo().frame(frameAuthorization);
-
-        sleep(1000);
-        WebElement editLogin = driver.findElement(By.name("username"));
-        WebElement btnInputPass = driver.findElement(By.cssSelector("button[data-test-id='next-button']"));
-        editLogin.sendKeys("klyushkindimatest");
-        btnInputPass.click();
-
-        sleep(1000);
-        WebElement editPass = driver.findElement(By.name("password"));
-        WebElement btnLogin = driver.findElement(By.cssSelector("button[data-test-id='submit-button']"));
-        editPass.sendKeys("autotest_1234");
-        btnLogin.click();
-        sleep(5000);
-
-        //Assert, что вход выполнен успешно
-        assertTrue(driver.getTitle().contains("Входящие - Почта Mail.ru"), "Вход в почту не выполнен");
-
         //Создать новое письмо (заполнить адресата (самого себя), тему письма и тело)
         WebElement btnNewMessage = driver.findElement(By.cssSelector("a[title='Написать письмо']"));
         btnNewMessage.click();
-        sleep(1000);
         WebElement editMessageReceiver = driver.findElement(By.cssSelector("div.inputContainer--nsqFu > input"));
         editMessageReceiver.sendKeys("klyushkindimatest@mail.ru");
         WebElement editMessageTitle = driver.findElement(By.cssSelector("input[name='Subject']"));
@@ -68,18 +25,20 @@ public class DeleteMessageTest {
         //Отправить письмо
         WebElement btnSendMessage = driver.findElement(By.cssSelector("button[data-test-id='send']"));
         btnSendMessage.click();
-        sleep(500);
+        WebElement infoSendMessage = driver.findElement(By.xpath("//span[@title='Закрыть']"));
+        infoSendMessage.click();
 
         //Verify, что письмо появилось в папке Входящие
-        driver.navigate().to("https://e.mail.ru/tomyself/");
-        sleep(5000);
+        WebElement linkIncomingMail = driver.findElement(By.cssSelector("a.nav__item[title='Входящие']"));
+        linkIncomingMail.click();
+        WebElement linkTomyself = driver.findElement(By.xpath("//span[contains(.,'Письма себе')]/ancestor::a"));
+        linkTomyself.click();
         var message =
             driver.findElements(By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]"));
         assertEquals(message.size(), 1, "Письмо не найдено в папке Входящие");
 
         //Verify контент, адресата и тему письма (должно совпадать с пунктом 3)
         message.get(0).click();
-        sleep(1000);
         editMessageReceiver = driver.findElement(By.cssSelector("span.letter-contact"));
         assertEquals(editMessageReceiver.getAttribute("Title"), "klyushkindimatest@mail.ru",
             "Адресат письма не совпадает");
@@ -91,20 +50,12 @@ public class DeleteMessageTest {
         //Удалить письмо
         WebElement btnDel = driver.findElement(By.xpath("//div[@class='button2__txt' and contains(.,'Удалить')]"));
         btnDel.click();
-        sleep(1000);
 
         //Verify что письмо появилось в папке Корзина
-        driver.navigate().to("https://e.mail.ru/trash/");
-        sleep(5000);
+        WebElement linkTrash = driver.findElement(By.cssSelector("a.nav__item[title='Корзина']"));
+        linkTrash.click();
         message =
             driver.findElements(By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]"));
         assertEquals(message.size(), 1, "Письмо не найдено в корзине");
-
-        //Выйти из учётной записи
-        WebElement btnAccount = driver.findElement(By.cssSelector("div[data-testid='whiteline-account']"));
-        btnAccount.click();
-        sleep(1000);
-        WebElement btnExit = driver.findElement(By.cssSelector("div[data-testid='whiteline-account-exit']"));
-        btnExit.click();
     }
 }
--- src/test/java/ru/levelp/at/homework3/DraftMailTest.java
+++ src/test/java/ru/levelp/at/homework3/DraftMailTest.java
@@ -2,61 +2,19 @@ package ru.levelp.at.homework3;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-import static ru.levelp.at.utils.SleepUtils.sleep;
 
 import java.time.LocalDateTime;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
-public class DraftMessageTest {
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeMethod() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        driver.quit();
-    }
-
+public class DraftMailTest extends MailTestBase {
     @Test
     public void testDraftMessage() {
-        //Войти в почту
-        driver.navigate().to("https://mail.ru/");
-        WebElement btnAuthorization = driver.findElement(By.className("ph-login"));
-        btnAuthorization.click();
-
-        sleep(1000);
-        WebElement frameAuthorization = driver.findElement(By.className("ag-popup__frame__layout__iframe"));
-        driver.switchTo().frame(frameAuthorization);
-
-        sleep(1000);
-        WebElement editLogin = driver.findElement(By.name("username"));
-        WebElement btnInputPass = driver.findElement(By.cssSelector("button[data-test-id='next-button']"));
-        editLogin.sendKeys("klyushkindimatest");
-        btnInputPass.click();
-
-        sleep(1000);
-        WebElement editPass = driver.findElement(By.name("password"));
-        WebElement btnLogin = driver.findElement(By.cssSelector("button[data-test-id='submit-button']"));
-        editPass.sendKeys("autotest_1234");
-        btnLogin.click();
-        sleep(5000);
-
-        //Assert, что вход выполнен успешно
-        assertTrue(driver.getTitle().contains("Входящие - Почта Mail.ru"), "Вход в почту не выполнен");
-
         //Создать новое письмо (заполнить адресата, тему письма и тело)
         WebElement btnNewMessage = driver.findElement(By.cssSelector("a[title='Написать письмо']"));
         btnNewMessage.click();
-        sleep(1000);
         WebElement editMessageReceiver = driver.findElement(By.cssSelector("div.inputContainer--nsqFu > input"));
         editMessageReceiver.sendKeys("klyushkindimatest@mail.ru");
         WebElement editMessageTitle = driver.findElement(By.cssSelector("input[name='Subject']"));
@@ -68,21 +26,21 @@ public class DraftMessageTest {
         //Сохранить его как черновик
         WebElement btnSaveMessage = driver.findElement(By.cssSelector("button[data-test-id='save']"));
         btnSaveMessage.click();
-        sleep(500);
 
         //Verify, что письмо сохранено в черновиках
         var notifyDraft = driver.findElements(
             By.xpath("//span[@class='notify__message__text' and contains(.,'Сохранено в черновиках')]"));
         assertTrue(notifyDraft.size() > 0, "Письмо не сохранено в черновики");
-        driver.navigate().to("https://e.mail.ru/drafts/");
-        sleep(5000);
+        WebElement infoSendMessage = driver.findElement(By.xpath("//button[@title='Закрыть']"));
+        infoSendMessage.click();
+        WebElement linkDrafts = driver.findElement(By.cssSelector("a.nav__item[title='Черновики']"));
+        linkDrafts.click();
         var message =
             driver.findElements(By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]"));
         assertEquals(message.size(), 1, "Письмо не сохранено в черновики");
 
         //Verify контент, адресата и тему письма (должно совпадать с пунктом 3)
         message.get(0).click();
-        sleep(1000);
         editMessageReceiver =
             driver.findElement(By.cssSelector("div[class='contactsContainer--3RMuQ'] span[class='text--1tHKB']"));
         assertEquals(editMessageReceiver.getText(), "klyushkindimatest@mail.ru", "Адресат письма не совпадает");
@@ -94,27 +52,19 @@ public class DraftMessageTest {
         //Отправить письмо
         WebElement btnSendMessage = driver.findElement(By.cssSelector("button[data-test-id='send']"));
         btnSendMessage.click();
-        sleep(500);
+        infoSendMessage = driver.findElement(By.xpath("//span[@title='Закрыть']"));
+        infoSendMessage.click();
 
         //Verify, что письмо исчезло из черновиков
-        driver.navigate().to("https://e.mail.ru/drafts/");
-        sleep(5000);
-        message =
-            driver.findElements(By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]"));
-        assertEquals(message.size(), 0, "Письмо осталось в черновиках");
+        boolean deleteDraftMessage = wait.until(ExpectedConditions.invisibilityOfElementLocated(
+            By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]")));
+        assertTrue(deleteDraftMessage, "Письмо осталось в черновиках");
 
         //Verify, что письмо появилось в папке отправленные
-        driver.navigate().to("https://e.mail.ru/sent/");
-        sleep(5000);
+        WebElement linkSent = driver.findElement(By.cssSelector("a.nav__item[title='Отправленные']"));
+        linkSent.click();
         message =
             driver.findElements(By.xpath("//span[@class='ll-sj__normal' and contains(.,'Типо тема " + dateNow + "')]"));
         assertEquals(message.size(), 1, "Письмо не найдено в отправленных");
-
-        //Выйти из учётной записи
-        WebElement btnAccount = driver.findElement(By.cssSelector("div[data-testid='whiteline-account']"));
-        btnAccount.click();
-        sleep(1000);
-        WebElement btnExit = driver.findElement(By.cssSelector("div[data-testid='whiteline-account-exit']"));
-        btnExit.click();
     }
 }
